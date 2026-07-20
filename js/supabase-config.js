be030b7b8ef178e5df83f173528c6306b5e8846a
ro <rodrigoverasantis@gmail.com>
2026-07-19 21:58:00 -0400

    Config: actualizar Supabase URL

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
-var SUPABASE_URL = 'https://ebfnusykfvcardunmilq.supabase.co';
+var SUPABASE_URL = 'https://kxacuszfhyhxngeazuze.supabase.co';
 var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImViZm51c3lrZnZjYXJkdW5taWxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQzMTk5NzQsImV4cCI6MjA5OTg5NTk3NH0.r6vvxtNutgkEc9thD4IUucpVzxGLHkE7_ovgoqKMUd8';
 
 var supabaseClient = null;
